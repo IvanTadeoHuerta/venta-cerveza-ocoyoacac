b42e38c6d0d86e987224080b92b2f444d50fffe0
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2019-07-10 12:54:20 -0500

    Registro y generacion de token [ ESTABLE ]

--- js/app.js
+++ js/app.js
@@ -12,43 +12,47 @@ if (navigator.serviceWorker) {
 
   // window.addEventListener('load', () => {
 
-    navigator.serviceWorker.register(swLocation).then(registration => {
-      console.log('registrado')
-      messaging.useServiceWorker(registration);
+  navigator.serviceWorker.register(swLocation).then(registration => {
+    console.log('registrado')
+    messaging.useServiceWorker(registration);
+
+
+    // Add the public key generated from the console here.
+    messaging.usePublicVapidKey("BP-2K1ewpmeAkqn6RtX9vkb1zFlbNwbOjOT_5dHGBg5UzTZKi-zuw_mXQKjXqse6XRnUv4-RRy-3fmnFHxVa6ng");
+
+    messaging.requestPermission().then(function () {
+      console.log('Notification permission granted.');
+      // TODO(developer): Retrieve an Instance ID token for use with FCM.
+      // ...
+    }).catch(function (err) {
+      console.log('Unable to get permission to notify.', err);
     });
+
+
+    // Get Instance ID token. Initially this makes a network call, once retrieved
+    // subsequent calls to getToken will return from cache.
+    messaging.getToken().then(function (currentToken) {
+      console.log(currentToken)
+      if (currentToken) {
+        //   sendTokenToServer(currentToken);
+        //   updateUIForPushEnabled(currentToken);
+      } else {
+        // Show permission request.
+        console.log('No Instance ID token available. Request permission to generate one.');
+        // Show permission UI.
+        //   updateUIForPushPermissionRequired();
+        //   setTokenSentToServer(false);
+      }
+    }).catch(function (err) {
+      console.log('An error occurred while retrieving token. ', err);
+      // showToken('Error retrieving Instance ID token. ', err);
+      // setTokenSentToServer(false);
+    });
+
+
+  });
 
   // });
 
 }
 
-// Add the public key generated from the console here.
-messaging.usePublicVapidKey("BP-2K1ewpmeAkqn6RtX9vkb1zFlbNwbOjOT_5dHGBg5UzTZKi-zuw_mXQKjXqse6XRnUv4-RRy-3fmnFHxVa6ng");
-
-messaging.requestPermission().then(function () {
-  console.log('Notification permission granted.');
-  // TODO(developer): Retrieve an Instance ID token for use with FCM.
-  // ...
-}).catch(function (err) {
-  console.log('Unable to get permission to notify.', err);
-});
-
-
-// Get Instance ID token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
-messaging.getToken().then(function (currentToken) {
-  console.log(currentToken)
-  if (currentToken) {
-    //   sendTokenToServer(currentToken);
-    //   updateUIForPushEnabled(currentToken);
-  } else {
-    // Show permission request.
-    console.log('No Instance ID token available. Request permission to generate one.');
-    // Show permission UI.
-    //   updateUIForPushPermissionRequired();
-    //   setTokenSentToServer(false);
-  }
-}).catch(function (err) {
-  console.log('An error occurred while retrieving token. ', err);
-  // showToken('Error retrieving Instance ID token. ', err);
-  // setTokenSentToServer(false);
-});
